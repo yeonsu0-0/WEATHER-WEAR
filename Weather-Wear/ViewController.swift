b4ed9199e1b036f427d8935d4b90701470bbfcb4
yeonsu <drawingbonny@gmail.com>
2023-01-21 22:56:17 +0900

    first commit

---
+++ Weather-Wear/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-Wear
+//
+//  Created by yeonsu on 2023/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
