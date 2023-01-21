5d4441386e657916574576d2d53370fbad5f2a6b
yeonsu <drawingbonny@gmail.com>
2023-01-22 00:51:16 +0900

    feat: Add bottom tab bar navigation

---
+++ Weather-Wear/MyPage/MyPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyPageViewController.swift
+//  Weather-Wear
+//
+//  Created by yeonsu on 2023/01/22.
+//
+
+import UIKit
+
+class MyPageViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
