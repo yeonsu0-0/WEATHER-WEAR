385fd11facdc75da5056df089030e21d57a9a60c
ideajimin <ideajimin@gmail.com>
2023-01-22 21:31:26 +0900

    컨트롤러 연결
    탭바 및 컨트롤러 연결

---
+++ Weather-Wear/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Weather-Wear
+//
+//  Created by 이지민 on 2023/01/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
