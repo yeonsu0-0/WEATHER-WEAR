ed183ce685b1bfe2a62d92d9d096b5e9a2c14a6e
yeonsu <drawingbonny@gmail.com>
2023-01-25 01:40:15 +0900

    feat: Add WatchViewController

---
+++ Weather-Wear/Watch/WatchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WatchViewController.swift
+//  Weather-Wear
+//
+//  Created by yeonsu on 2023/01/25.
+//
+
+import UIKit
+
+class WatchViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
