06f01437003f6e3718447e7c9141cd7cfc544790
yeonsu <drawingbonny@gmail.com>
2023-01-25 11:15:08 +0900

    feat: Add KaKao logout function

--- Weather-Wear/MyPage/MyPageViewController.swift
+++ Weather-Wear/MyPage/MyPageViewController.swift
@@ -6,8 +6,23 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 class MyPageViewController:UIViewController {
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("logout() success.")
+            }
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
